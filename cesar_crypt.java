f387c14cce5242a3c94a342862fab8ac113b0c63
0x13A0F <raoufbarcelona@gmail.com>
2016-12-11 22:19:29 +0100

    Update and rename cesar.java to cesar_crypt.java

--- cesar_crypt.java
+++ cesar_crypt.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-public class cesar {
+public class cesar_crypt {
     public static void main(String[] args){
       System.out.println("Message to enter: ");
       Scanner sc = new Scanner(System.in);
       String ciphertext="";
       String message = sc.nextLine().toUpperCase().replaceAll("[^A-Z]","");
       System.out.println("Key (1-25): ");
-      sc.reset();
       int key = sc.nextInt();
       for (int i=0; i < message.length(); i++){
         int m = message.charAt(i) - 65;
